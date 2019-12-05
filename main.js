73ac8ad0737065a286eb919e6c15a73a2f648b96
YutaTanaka <yuta84q.ihcarok@gmail.com>
2019-12-05 12:02:32 +0900

    update js

--- main.js
+++ main.js
@@ -5,15 +5,11 @@ document.addEventListener('DOMContentLoaded', function(){
 				window.addEventListener('devicemotion', e => {
 					const acc = e.acceleration;
 					const accg = e.accelerationIncludingGravity;
-					document.getElementById("accg-x").innerHTML = accg.x.toFixed(3);
-					document.getElementById("accg-y").innerHTML = accg.y.toFixed(3);
-					document.getElementById("accg-z").innerHTML = accg.z.toFixed(3);
-					const g = ((accg.x ** 2) + (accg.y ** 2) + (accg.z ** 2)) ** 0.5;
-					const gp = (((accg.x + acc.x) ** 2) + ((accg.y + acc.y) ** 2) + ((accg.z + acc.z) ** 2)) ** 0.5;
+					document.getElementById("acc-x").innerHTML = (accg.x - acc.x).toFixed(6);
+					document.getElementById("acc-y").innerHTML = (accg.y - acc.y).toFixed(6);
+					document.getElementById("acc-z").innerHTML = (accg.z - acc.z).toFixed(6);
 					const gm = (((accg.x - acc.x) ** 2) + ((accg.y - acc.y) ** 2) + ((accg.z - acc.z) ** 2)) ** 0.5;
-					document.getElementById("grav" ).innerHTML = g.toFixed(3);
-					document.getElementById("gravp" ).innerHTML = gp.toFixed(3);
-					document.getElementById("gravm" ).innerHTML = gm.toFixed(3);
+					document.getElementById("grav" ).innerHTML = gm.toFixed(6);
 				})
 			} else {
 				// 許可を得られなかった場合の処理
