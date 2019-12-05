85b6c9f9e1213c156e2609e695748a1fe7aa378e
YutaTanaka <yuta84q.ihcarok@gmail.com>
2019-12-05 12:36:49 +0900

    update js

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ document.addEventListener('DOMContentLoaded', function(){
 		DeviceMotionEvent.requestPermission().then(permissionState => {
 			if (permissionState === 'granted') {
 				window.addEventListener('devicemotion', e => {
+					const RAD_TO_DEG = 180 / Math.PI;
 					const acc = e.acceleration;
 					const accg = e.accelerationIncludingGravity;
+					const x = accg.x - acc.x;
+					const y = accg.y - acc.y;
+					const z = accg.z - acc.z;
+					const g = (x**2 + y**2 + z**2) ** 0.5;
+					const rotzx = Math.atan(x/z) * RAD_TO_DEG;
+					const rotzy = Math.atan(y/z) * RAD_TO_DEG;
 					document.getElementById("acc-x").innerHTML = (accg.x - acc.x).toFixed(6);
 					document.getElementById("acc-y").innerHTML = (accg.y - acc.y).toFixed(6);
 					document.getElementById("acc-z").innerHTML = (accg.z - acc.z).toFixed(6);
-					const g = (((accg.x - acc.x) ** 2) + ((accg.y - acc.y) ** 2) + ((accg.z - acc.z) ** 2)) ** 0.5;
 					document.getElementById("grav" ).innerHTML = g.toFixed(6);
+					document.getElementById("rotxz").innerHTML = rotzx.toFixed(6);
+					document.getElementById("rotyz").innerHTML = rotzy.toFixed(6);
 				})
 			} else {
 				// 許可を得られなかった場合の処理
