1465a8a35955cc5c6411ee4254921658e48532e9
YutaTanaka <yuta84q.ihcarok@gmail.com>
2019-12-05 11:56:39 +0900

    update js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ document.addEventListener('DOMContentLoaded', function(){
 					document.getElementById("accg-y").innerHTML = accg.y.toFixed(3);
 					document.getElementById("accg-z").innerHTML = accg.z.toFixed(3);
 					const g = ((accg.x ** 2) + (accg.y ** 2) + (accg.z ** 2)) ** 0.5;
-					const gp = (((accg.x + accg.x) ** 2) + ((accg.y + acc.y) ** 2) + ((accg.z + acc.z) ** 2)) ** 0.5;
-					const gm = (((accg.x - accg.x) ** 2) + ((accg.y - acc.y) ** 2) + ((accg.z - acc.z) ** 2)) ** 0.5;
+					const gp = (((accg.x + acc.x) ** 2) + ((accg.y + acc.y) ** 2) + ((accg.z + acc.z) ** 2)) ** 0.5;
+					const gm = (((accg.x - acc.x) ** 2) + ((accg.y - acc.y) ** 2) + ((accg.z - acc.z) ** 2)) ** 0.5;
 					document.getElementById("grav" ).innerHTML = g.toFixed(3);
 					document.getElementById("gravp" ).innerHTML = gp.toFixed(3);
 					document.getElementById("gravm" ).innerHTML = gm.toFixed(3);
