e6ab5a2582b333051cedf96829efe7d428ac8ccc
YutaTanaka <yuta84q.ihcarok@gmail.com>
2019-12-06 09:52:24 +0900

    update js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', function(){
-	let alpha, tiltx, tilty;
+	const RAD_TO_DEG = 180 / Math.PI;
+	let alpha = 180, tiltx, tilty = 0;
 	const drawCanvas = () => {
 		const canvas = document.getElementById('canvas');
 		const context = canvas.getContext('2d');
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		context.beginPath();
-		context.arc(110, 110, 50, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+		context.arc(110, 110, 100, -90 / RAD_TO_DEG, (alpha - 90) / RAD_TO_DEG, false );
 
 		// 塗りつぶしの色
-		context.fillStyle = "rgba(255,0,0,0.8)";
+		context.fillStyle = "rgba(0,0,0,0)";
 		// // 塗りつぶしを実行
 		context.fill();
 		// 線の色
@@ -19,32 +20,31 @@ document.addEventListener('DOMContentLoaded', function(){
 		// 線を描画を実行
 		context.stroke();
 
-		context.fillText(tiltx ? tiltx.toFixed(6) : "tiltx", 10, 10);
-		context.fillText(alpha ? alpha.toFixed(6) : "alpha", 10, 30);
+		//context.fillText(tiltx ? tiltx.toFixed(6) : "tiltx", 10, 10);
+		//context.fillText(alpha ? alpha.toFixed(6) : "alpha", 10, 30);
 	};
 
 	const requestDevicePermission = () => {
 
 		const addOrientationEvent = () => {
 			window.addEventListener('deviceorientation', e => {
-				alpha = e.alpha;
 				if(e.alpha)
 				{
-					document.getElementById("rot").innerHTML = alpha.toFixed(3);
+					alpha = e.alpha;
+					document.getElementById("rot").innerHTML = alpha
 				}
 			})
 		};
 
 		const addMotionEvent = () => {
 			window.addEventListener('devicemotion', e => {
-				const RAD_TO_DEG = 180 / Math.PI;
 				const x = e.accelerationIncludingGravity.x - e.acceleration.x;
 				const y = e.accelerationIncludingGravity.y - e.acceleration.y;
 				const z = e.accelerationIncludingGravity.z - e.acceleration.z;
 				tiltx = Math.atan(x/z);
 				tilty = Math.atan(y/z);
-				document.getElementById("tiltx").innerHTML = (tiltx * RAD_TO_DEG).toFixed(6);
-				document.getElementById("tilty").innerHTML = (tilty * RAD_TO_DEG).toFixed(6);
+				document.getElementById("tiltx").innerHTML = (tiltx * RAD_TO_DEG)
+				document.getElementById("tilty").innerHTML = (tilty * RAD_TO_DEG)
 			})
 		};
 
