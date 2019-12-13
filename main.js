8552afc56a42a26a2beadbacef8444b8deb4289e
YutaTanaka <yuta84q.ihcarok@gmail.com>
2019-12-13 09:02:49 +0900

    update js

--- main.js
+++ main.js
@@ -59,12 +59,6 @@ class OrientationEvent
 		this.alpha_pre = 0;
 		this.ng_num = 0;
 		this.correction = 0;
-		this.start_rotation = 0;
-	}
-
-	setStart()
-	{
-		this.start_rotation = this.rotation;
 	}
 
 	addEventListener()
@@ -126,8 +120,31 @@ class MotionEvent
 {
 	constructor()
 	{
-		this.tiltx = 0;
-		this.tilty = 0;
+		this.current_tiltx = 0;
+		this.current_tilty = 0;
+		this.start_tiltx = 0;
+		this.start_tilty = 0;
+		this.stop_tiltx = 0;
+		this.stop_tilty = 0;
+	}
+
+	setStart()
+	{
+		this.start_tiltx = this.current_tiltx;
+		this.start_tilty = this.current_tilty;
+	}
+
+	setStop()
+	{
+		this.stop_tiltx = this.current_tiltx;
+		this.stop_tilty = this.current_tilty;
+	}
+
+	getTilt()
+	{
+		const tiltx = this.stop_tiltx - start_tiltx;
+		const tiltx = this.stop_tilty - start_tilty;
+		return {tiltx, tilty};
 	}
 
 	addEventListener()
@@ -138,10 +155,10 @@ class MotionEvent
 			const x = e.accelerationIncludingGravity.x - e.acceleration.x;
 			const y = e.accelerationIncludingGravity.y - e.acceleration.y;
 			const z = e.accelerationIncludingGravity.z - e.acceleration.z;
-			this.tiltx = Math.atan(x/z);
-			this.tilty = Math.atan(y/z);
-			document.getElementById("tiltx").innerHTML = (this.tiltx * RAD_TO_DEG)
-			document.getElementById("tilty").innerHTML = (this.tilty * RAD_TO_DEG)
+			this.current_tiltx = Math.atan(x/z);
+			this.current_tilty = Math.atan(y/z);
+			document.getElementById("tiltx").innerHTML = (this.current_tiltx * RAD_TO_DEG)
+			document.getElementById("tilty").innerHTML = (this.current_tilty * RAD_TO_DEG)
 		});
 	}
 
@@ -173,10 +190,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	const orientationEvent = new OrientationEvent();
 	const motionEvent = new MotionEvent();
 
-	let start_rot, stop_rot;
-	let start_tiltx, stop_tiltx;
-	let start_tilty, stop_tilty;
-
 	const start_measuring = (e) => {
 		orientationEvent.addEvent();
 		motionEvent.addEvent();
@@ -185,22 +198,14 @@ document.addEventListener('DOMContentLoaded', () => {
 		document.getElementById("start-button").disabled = true;
 
 		setTimeout(() => {
-			orientationEvent.setStart();
-			start_rot = orientationEvent.rotation;
-			start_tiltx = motionEvent.tiltx;
-			start_tilty = motionEvent.tilty;
-			alert(start_tiltx);
+			motionEvent.setStart();
 			document.getElementById("stop-button").disabled = false;
 		}, 1000);
 	};
 
 	const stop_measuring = (e) => {
-		stop_rot = orientationEvent.rotation;
-		stop_tiltx = motionEvent.tiltx;
-		stop_tilty = motionEvent.tilty;
-		const rot = Math.abs(stop_rot - start_rot);
-		const tiltx = (start_tiltx + stop_tiltx) / 2;
-		const tilty = (start_tilty + stop_tilty) / 2;
+		motionEvent.setStop();
+		const {tiltx, tilty} = motionEvent.getTilt();
 		alert(" " + rot + "\n" + tiltx + "\n" + tilty);
 	};
 
